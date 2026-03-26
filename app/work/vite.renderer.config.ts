947a699096a6f05b77f5c0621b6627601d78740e
liujinglun1 <liujinglun1.wps.cn>
2026-03-26 18:12:01 +0800

    feat: 修复 dev 问题

--- app/work/vite.renderer.config.ts
+++ app/work/vite.renderer.config.ts
@@ -6,6 +6,11 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config
 export default defineConfig({
+  server: {
+    forwardConsole: {
+      unhandledErrors: false,
+    },
+  },
   css: {
     postcss: {
       plugins: [tailwindcss() as any, autoprefixer()],
