4c65e1b6eeeff95bbc81ec05fdea838c629eb841
liujinglun1 <liujinglun1.wps.cn>
2026-04-12 18:03:48 +0800

    feat: 修复顶栏样式

--- app/work/src/renderer/src/components/base/MainTitle.vue
+++ app/work/src/renderer/src/components/base/MainTitle.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useSidebar } from "@/components/ui/sidebar";
+const { state } = useSidebar();
+
+const isCollapsed = computed(() => {
+  return state.value === "collapsed";
+});
+</script>
 <template>
-  <div class="flex w-full flex-col items-center">
+  <div
+    class="flex w-full flex-col items-center transition-all duration-300"
+    :class="{ 'pl-[85px]': isCollapsed }"
+  >
     <div class="drag-region h-2 w-full"></div>
     <div class="flex h-9 w-full items-center gap-1 px-2 pb-2">
       <slot />
@@ -8,5 +20,3 @@
     </div>
   </div>
 </template>
-
-<script setup lang="ts"></script>
